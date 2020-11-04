0c7ea2c7bdb830681d7734722807b25a451a0c32
Valerii <valerii.tudose@ucms.ro>
2020-11-04 21:46:10 +0200

    add special backgrounds

--- site.js
+++ site.js
@@ -1,21 +1,51 @@
 var prevSelection;
 onVoteClick = (selection) => {
-    if (selection !== prevSelection)
-        reset();
 
-    var element = document.querySelector(".candidate-" + selection + " .round-frame");
+    if (selection === prevSelection) {
+        reset(selection);
+        prevSelection = undefined;
+        return;
+    }
+
+    removeOther(selection);
+
+    var element = document.querySelector(".container-" + selection + " .round-frame");
     element.classList.toggle("text-aligner");
     element.children[0].classList.toggle("hide");
     prevSelection = selection;
-
 }
 
-reset = () => {
-    var element = document.querySelector(".candidate-" + prevSelection + " .round-frame");
+reset = (selection) => {
+    var opposite = selection === "one" ? "two" : "one";
+    var element = document.querySelector(".container-" + opposite);
+    element.classList.toggle("hide");
+    element.style.opacity = "1";
 
-    if (!element)
-        return;
+    var frame = document.querySelector(".container-" + selection + " .round-frame");
+    frame.classList.toggle("text-aligner");
+    frame.children[0].classList.toggle("hide");
 
-    element.classList.remove("text-aligner");
-    element.children[0].classList.add("hide");
+    var bkg = document.querySelector(".background");
+    bkg.classList.remove("bag");
+    bkg.classList.remove("pas");
+}
+
+removeOther = (selection) => {
+    var opposite = selection === "one" ? "two" : "one";
+    var element = document.querySelector(".container-" + opposite);
+    element.style.opacity = "0";
+    element.addEventListener('transitionend', () => {
+        element.classList.add("hide");
+        changeBackground(selection);
+    });
+}
+
+changeBackground = (selection) => {
+    var bkg = document.querySelector(".background");
+    if (selection == "one") {
+        bkg.classList.add("bag");
+    }
+    else {
+        bkg.classList.add("pas");
+    }
 }
