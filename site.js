fbed334c4a4efe0924ac8da4e38513da5028d9e0
Valerii <valerii.tudose@ucms.ro>
2020-11-04 20:09:52 +0200

    add ability to vote for a candidate

--- site.js
+++ site.js
@@ -1,1 +1,18 @@
-// alert("aaa");
+var prevSelection;
+onVoteClick = (s) => {
+    reset();
+    var element = document.querySelector(".candidate-" + s + " .round-frame");
+    element.classList.add("text-aligner");
+    element.children[0].classList.remove("hide");
+    prevSelection = s;
+
+}
+
+reset = () => {
+    var element = document.querySelector(".candidate-" + prevSelection + " .round-frame");
+
+    if (!element)
+        return;
+    element.classList.remove("text-aligner");
+    element.children[0].classList.add("hide");
+}
